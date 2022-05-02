4529ddebf624d0b069f5e25c9af511eb74cba377
JustinAshmore22 <88673704+JustinAshmore22@users.noreply.github.com>
2022-05-02 11:59:06 -0500

    Co-authored-by: dholl205 <dholl205@users.noreply.github.com>

--- script.js
+++ script.js
@@ -190,8 +190,8 @@ function addItemToCart(title, price, image) {
       alert("This item is already added to the cart");
       return;
     }
-  }
-  var cartRowContents = ` 
+
+    var cartRowContents = ` 
   <div class="cart-item cart-column">
   <img class="cart-item-image" src="${image}" height="100">
   <span class="cart-item-title">${title}</span>
@@ -201,31 +201,33 @@ function addItemToCart(title, price, image) {
   <input class="cart-quantity-input" type="number" value="1">
   <button class="btn btn-danger" type="button">REMOVE</button>
 </div>`;
-  cartRow.innerHTML = cartRowContents;
-  cartItems.append(cartRow);
-  cartRow
-    .getElementsByClassName("btn-danger")[0]
-    .addEventListener("click", removeCartItem);
-  cartRow
-    .getElementsByClassName("cart-quantity-input")[0]
-    .addEventListener("change", quantityChanged);
-}
-
-function updateCartTotal() {
-  var cartItemContainer = document.getElementsByClassName("cart-items")[0];
-  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
-  var total = 0;
-  for (var i = 0; i < cartRows.length; i++) {
-    var cartRow = cartRows[i];
-    var priceElement = cartRow.getElementsByClassName("cart-price")[0];
-    var quantityElement = cartRow.getElementsByClassName(
-      "cart-quantity-input"
-    )[0];
-    var price = parseFloat(priceElement.innerText.replace("$", ""));
-    var quantity = quantityElement.value;
-    total = total + price * quantity;
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+    cartRow
+      .getElementsByClassName("btn-danger")[0]
+      .addEventListener("click", removeCartItem);
+    cartRow
+      .getElementsByClassName("cart-quantity-input")[0]
+      .addEventListener("change", quantityChanged);
   }
-  total = Math.round(total * 100) / 100;
-  document.getElementsByClassName("cart-total-price")[0].innerText =
-    "$" + total;
+
+  function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+    var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+    var total = 0;
+    for (var i = 0; i < cartRows.length; i++) {
+      var cartRow = cartRows[i];
+
+      var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+      var quantityElement = cartRow.getElementsByClassName(
+        "cart-quantity-input"
+      )[0];
+      var price = parseFloat(priceElement.innerText.replace("$", ""));
+      var quantity = quantityElement.value;
+      total = total + price * quantity;
+    }
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName("cart-total-price")[0].innerText =
+      "$" + total;
+  }
 }
